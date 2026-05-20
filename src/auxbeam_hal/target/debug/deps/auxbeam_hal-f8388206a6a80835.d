1499f4bd98ece64c578e7857211f6a5bf6a61506
Cole Nichols <colednichols@gmail.com>
2026-05-19 21:34:31 -0700

    chore: remove compiled target artifacts from tracking

--- src/auxbeam_hal/target/debug/deps/auxbeam_hal-f8388206a6a80835.d
+++
@@ -1,5 +0,0 @@
-/home/coled/code/auxbeam-8gang-uart/src/auxbeam_hal/target/debug/deps/auxbeam_hal-f8388206a6a80835.d: src/main.rs
-
-/home/coled/code/auxbeam-8gang-uart/src/auxbeam_hal/target/debug/deps/libauxbeam_hal-f8388206a6a80835.rmeta: src/main.rs
-
-src/main.rs:
